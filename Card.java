445ee464d286165b6b8585694eb5a21c37782e6b
Andrew Kulpa <aj-kulpa@wiu.edu>
2017-03-31 11:23:51 -0500

    Create Card.java

---
+++ Card.java
@@ -0,0 +1,73 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Game;
+
+/**
+ *
+ * @author Andrew Kulpa
+ */
+public class Card {
+    private int cardValue;
+    private String cardType;
+    private int level;
+  	private String icon;
+  	private String currentPosition;
+    
+    public Card (int faceValue, String suit){
+        this.faceValue = faceValue;
+        this.suit = suit;
+    }
+    
+    public int getCardValue(){
+        return this.cardValue;
+    }
+    
+    public String getCardType(){
+        return this.cardType;
+    }
+    
+    public int getLevel(){
+		return this.level;
+    }
+  	
+  	public String getCurrentPosition(){
+        return this.currentPosition;
+    }
+  	
+    public int getIcon(){
+		return this.icon;
+    }
+    
+  	/*
+    public String toString(){
+        return this.suit + " :: " + this.faceValue;
+    }
+    
+    public String getImage(){
+        String imageName = "";
+        if(this.faceValue < 11 && this.faceValue > 1){
+            return this.faceValue + this.suit;
+        }
+        else if(this.faceValue == 1){
+            return "Ace" + this.suit;
+            //Ace
+        }
+        else if(this.faceValue == 11){
+            return "Jack" + this.suit;
+            //Jack
+        }
+        else if (this.faceValue == 12){
+            return "Queen" + this.suit;
+            //Queen
+        }
+        else if (this.faceValue == 13){
+            return "King" + this.suit;
+            //King
+        }
+        return imageName;
+    }
+    */
+}
